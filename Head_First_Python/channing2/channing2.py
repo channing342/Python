3e05e41cb9da627e517e7f8eebc74aba88919abd
ChanningLiu <channingliu@ChanningLiudeMacBook-Air.local>
2016-08-22 21:57:47 +0800

    Channing Moudle update Versin 1.2 , Fix API

--- Head_First_Python/channing2/channing2.py
+++ Head_First_Python/channing2/channing2.py
@@ -1,4 +1,4 @@
-def lol(the_list,level):
+def lol(the_list,level=0):
 	for each_item in the_list:
 		if isinstance (each_item,list):
 			lol (each_item,level+1)
@@ -9,4 +9,4 @@ def lol(the_list,level):
 
 noc = [ "Channing","Luke","Bii" , [ 27, 29 , 25 , [ "Supervised"  , "Senior" , "Junior" ] ] ]
 
-lol(noc,0)
+lol(noc)
