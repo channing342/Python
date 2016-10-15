0cc29cd4a59941d9cde0f45f2f30298b3341d5de
Channing <channing342@gmail.com>
2016-10-16 00:13:08 +0800

    Create sketch2.py

---
+++ Head_First_Python/chapter4/sketch2.py
@@ -0,0 +1,27 @@
+import os 
+os.chdir('/home/tomcat/Python/Head_First_Python/chapter4')
+
+man=[]
+other=[]
+try:
+        data = open('sketch.txt')
+        try: 
+                for each_line in data:
+                        (role , line_spoken) = each_line.split(":",1)
+                        line_spoken = line_spoken.strip()
+                        if role == 'Man':
+                                man.append(line_spoken)
+                        elif role == 'Other Man':
+                                other.append(line_spoken)
+        except ValueError:
+                pass
+        data.close()
+except IOError:
+        print('File is missing')
+
+try:
+        with open("man.txt","w") as out,open("other_man.txt","w") as out1
+        print(man,file=out)
+        print(other,file=out1)
+except IOError:
+        print('File error.')
