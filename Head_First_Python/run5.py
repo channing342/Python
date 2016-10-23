01c50b1365e32f490be5f166410a255f3556be09
Channing <channing342@gmail.com>
2016-10-23 23:03:22 +0800

    Create run5.py

---
+++ Head_First_Python/run5.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# coding: UTF-8
+
+
+def sanitize(time_string):
+        if '-' in time_string:
+                splitter = '-'
+        elif ':' in time_string:
+                splitter = ':'
+        else:
+                return(time_string)
+        (mins,secs) = time_string.split(splitter)
+        return(mins + '.' + secs)
+
+def get_file(the_file):
+        try:
+                with open(the_file) as file:
+                        data = file.readline()
+                return(data.strip().split(','))
+        except IOError as ioerror:
+                print('File error : ' +str(ioerror))
+                return(None)
+
+james = get_file('james.txt')
+julie = get_file('julie.txt')
+mikey = get_file('mikey.txt')
+sarah = get_file('sarah.txt')
