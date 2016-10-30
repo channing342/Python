0ab5ec28960decef56ec41b3b81cd66d729c9732
Channing <channing342@gmail.com>
2016-10-30 06:46:23 +0000

    Create class_2.py

---
+++ Head_First_Python/class_2.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+# coding: UTF-8
+
+class Athelet:
+        def __init__(self, a_name,a_Birthday=None,a_Time=[]):
+                self.name=a_name
+                self.birthday=a_Birthday
+                self.times=a_Time
+        def top3(self):
+            return (sorted([sanitize(t) for t in self.times])[0:3])
+        def add_time(self,time_value):
+                self.times.append(time_value)
+        def add_times(self,list_time_values):
+                self.times.extend(list_time_values)
+
+def sanitize(time_string):
+        if '-' in time_string:
+                splitter = '-'
+        elif ':' in time_string:
+                splitter = ':'
+        else:
+                return(time_string)
+        (mins,secs) = time_string.split(splitter)
+        return(mins + '.' + secs)
+
+def get_file(the_file):
+        try:
+                with open(the_file) as file:
+                        data = file.readline()
+                temp1=(data.strip().split(','))
+                return Athelet(temp1.pop(0),temp1.pop(0),temp1)
+
+        except IOError as ioerror:
+                print('File error : ' +str(ioerror))
+                return(None)
+
+sarah = get_file('sarah2.txt')
+james = get_file('james2.txt')
+julie = get_file('julie2.txt')
+mikey = get_file('mikey2.txt')
+
+print(sarah.name + "'s fastest times are : " + str(sarah.top3()))
+print(james.name + "'s fastest times are : " + str(james.top3()))
+print(julie.name + "'s fastest times are : " + str(julie.top3()))
+print(mikey.name + "'s fastest times are : " + str(mikey.top3()))
+
+
+channing=Athelet('Channing')
+channing.add_time('1.01')
+channing.add_times(['0.01','2-38','1.69'])
+
+print(channing.top3())
+class NamedList(list):
+     def __init__(self,a_name):
+             list.__init__([])
+             self.name=a_name
